376e2624a7d2a9be06ff94f1e295cfcd55286216
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-08-13 18:25:55 +0200

    Add file

---
+++ db/migrate/20220803054502_recipe_foods.rb
@@ -0,0 +1,10 @@
+class RecipeFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipe_foods do |t|
+      t.references :recipe, null: false, foreign_key: {to_table: 'recipes'}, index: true
+      t.references :food, null: false, foreign_key: {to_table: 'foods'}, index: true
+      t.integer :quantity, default: 0
+      t.timestamps
+    end
+  end
+end
