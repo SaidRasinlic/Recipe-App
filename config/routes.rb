b0fd69b4cf9b4e87243949fd8c91a84164fe5c99
udeaghad <dozie.udeagha@gmail.com>
2022-10-10 17:11:31 +0100

    update sign out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'user#index'
+  # root 'user#index'
 end
