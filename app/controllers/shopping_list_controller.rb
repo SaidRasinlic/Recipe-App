fb151a2add549a16ecd67b1ab6d70817198770e5
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-10-15 13:27:19 +0200

    Implement THIN CONTROLLER design pattern

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -29,17 +29,4 @@ class ShoppingListController < ApplicationController
     end
     [inventory, inventory_quantities]
   end
-
-  def build_shopping_list(recipe, inventory, list = [])
-    recipe.each do |recipe_item|
-      inventory_data = inventory.select { |inventory_item| inventory_item[:id] == recipe_item[:id] }
-      inventory_quantity = inventory_data.empty? ? 0 : inventory_data[0][:inventory_quantity]
-      shopping_list_quantity = recipe_item[:recipe_quantity] - inventory_quantity
-      if shopping_list_quantity.positive?
-        list.push({ name: recipe_item[:food_name], quantity: shopping_list_quantity, unit: recipe_item[:unit],
-                    price: recipe_item[:unit_price] * shopping_list_quantity })
-      end
-    end
-    list
-  end
 end
