4fddcec0cefb32ab93ca04583a63a338e37c8d58
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-10-15 02:17:54 +0200

    Add file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_142535) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_210538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,8 +18,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_142535) do
     t.string "name"
     t.string "measurement_unit"
     t.decimal "price"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -52,9 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_142535) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "preparation_time"
-    t.string "decimal"
-    t.decimal "cooking_time"
+    t.integer "preparation_time"
+    t.integer "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
@@ -64,22 +65,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_142535) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user_1 = User.create(name: 'Hailey Castro', email: 'user1@test.com', password: '1234567', password_confirmation: '1234567')
+user_2 = User.create(name: 'Ross Campbell', email: 'user2@test.com', password: '1234567', password_confirmation: '1234567')
+user_3 = User.create(name: 'Tristan Banks', email: 'user3@test.com', password: '1234567', password_confirmation: '1234567')
+user_4 = User.create(name: 'Tracey Thompson', email: 'user4@test.com', password: '1234567', password_confirmation: '1234567')
+
+# Recipe Seeds
+recipe_1 = Recipe.create(name: 'Artichokes', preparation_time: 5, cooking_time: 20, description: 'Simple and delicious steamed artichokes. Serve these with melted butter for dipping. You will need a pan with a steamer insert.', public: false, user: user_1)
+recipe_2 = Recipe.create(name: 'Avocado Deviled Eggs', preparation_time: 20, cooking_time: 20, description: 'This is a twist on the traditional deviled egg. I usually use 1 or 2 fewer yolks for the filling.', public: true, user: user_1)
+recipe_3 = Recipe.create(name: 'Slammin Salmon', preparation_time: 136, cooking_time: 8, description: 'An amazing culinary delight. Hands down the greatest grilled salmon ever. Easy and delicious.', public: true, user: user_3)
+recipe_4 = Recipe.create(name: 'Stuffed Peppers', preparation_time: 30, cooking_time: 65, description: 'Roasted green bell peppers are stuffed with feta cheese and a mixture of rice and green onions.', public: true, user: user_2)
+recipe_5 = Recipe.create(name: 'Quick Halushki', preparation_time: 75, cooking_time: 60, description: 'Polish dish combining bacon, fried cabbage and egg noodles.', public: false, user: user_2)
+recipe_6 = Recipe.create(name: 'Hasenpfeffer', preparation_time: 40, cooking_time: 90, description: 'Rabbit stew made with bacon, wine, garlic, shallots, other herbs and spices.', public: true, user: user_1)
+
+
+# Inventories
+inventory_1 = Inventory.create(name: 'Store', user_id: 1)
+inventory_2 = Inventory.create(name: 'Kitchen', user_id: 1)
+inventory_3 = Inventory.create(name: 'Store', user_id: 2)
+inventory_4 = Inventory.create(name: 'Kitchen', user_id: 2)
+
+# Foods
+food_1 = Food.create(name: 'Apple', measurement_unit: 'Kg', price: '5', user: user_1)
+food_2 = Food.create(name: 'Pineapple', measurement_unit: 'Kg', price: '4', user: user_1)
+food_3 = Food.create(name: 'Chicken breasts', measurement_unit: 'units', price: '2', user: user_1)
+food_4 = Food.create(name: 'Garlic', measurement_unit: 'grams', price: '0.1', user: user_2)
+food_5 = Food.create(name: 'Red Wine', measurement_unit: 'liter', price: '4', user: user_2)
+food_6 = Food.create(name: 'Bacon', measurement_unit: 'grams', price: '0.5', user: user_1)
+
+# RecipeFoods
+RecipeFood.create(quantity: 1, recipe: recipe_1, food: food_1 )
+RecipeFood.create(quantity: 50, recipe: recipe_1, food: food_4 )
+RecipeFood.create(quantity: 5, recipe: recipe_1, food: food_3 )
+RecipeFood.create(quantity: 1, recipe: recipe_2, food: food_1 )
+RecipeFood.create(quantity: 50, recipe: recipe_2, food: food_4 )
+RecipeFood.create(quantity: 5, recipe: recipe_2, food: food_3 )
+
+# InventoryFoods
+InventoryFood.create(quantity: 5, inventory: inventory_1, food: food_1)
+InventoryFood.create(quantity: 2, inventory: inventory_1, food: food_2)
+InventoryFood.create(quantity: 10, inventory: inventory_1, food: food_3)
+InventoryFood.create(quantity: 5, inventory: inventory_2, food: food_4)
+InventoryFood.create(quantity: 2, inventory: inventory_2, food: food_5)
+InventoryFood.create(quantity: 10, inventory: inventory_3, food: food_6)
